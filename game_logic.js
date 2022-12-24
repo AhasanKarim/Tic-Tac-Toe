ab4748b4dbd56ce840eebb68f06fd0331d39ed9a
Ahasan Karim <AhasanKarim@users.noreply.github.com>
2022-12-23 22:33:34 -0400

    logic left
    CSS and HTML finished

---
+++ game_logic.js
@@ -0,0 +1,10 @@
+/* Initalizing Variables */
+let playerText = document.getElementById('playerText')
+let restartBtn = gedocument.getElementById('restartBtn')
+let boxes = Array.from(getElementsByClassName('box'))
+
+console.log(boxes)
+
+
+const O_Letter ="O"
+co
